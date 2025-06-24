2d546c5d3f4ad3aed906351bd86a658c68d449e5
MhmmdRafli <122224190+VladR382@users.noreply.github.com>
2025-06-24 22:36:21 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-// Gunakan informasi kredensial dari InfinityFree
-$db_host = "sql107.infinityfree.com";        
-$db_user = "if0_39312582";                   
-$db_pass = "rafli6636";       
-$db_name = "if0_39312582_kantin";            
+// Ganti dengan kredensial database Anda
+$db_host = "localhost";        
+$db_user = "username_database_anda";                   
+$db_pass = "password_database_anda";       
+$db_name = "nama_database_anda";            
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
